ac9bfccfaaac82e5d7d9046a67f927951777fa73
Kunstbanause <kunstbanausen@googlemail.com>
2023-09-19 21:26:02 +0200

    Dead Zone for Stick Input
    Added a small amount of "dead zone" for the sticks, so the old n64 controllers do not drift so much

--- src/scene/scene.c
+++ src/scene/scene.c
@@ -165,14 +165,30 @@ void sceneUpdate(struct Scene* scene) {
 
     OSContPad* pad = controllersGetControllerData(0);
 
+    // Define a deadzone threshold (adjust this value as needed)
+    float deadzoneThreshold = 10.0f;
+
+    // Apply deadzone to stick input
+    float stickX = pad->stick_x;
+    float stickY = pad->stick_y;
+
+    // Ignore values within the deadzone
+    if (stickX >= -deadzoneThreshold && stickX <= deadzoneThreshold) {
+        stickX = 0.0f; // Ignore small stick movements
+    }
+
+    if (stickY >= -deadzoneThreshold && stickY <= deadzoneThreshold) {
+        stickY = 0.0f; // Ignore small stick movements
+    }
+
     // yaw
     struct Quaternion deltaRotate;
-    quatAxisAngle(&gUp, -pad->stick_x * ROTATE_SPEED * FIXED_DELTA_TIME * (1.0f / 80.0f), &deltaRotate);
+    quatAxisAngle(&gUp, -stickX * ROTATE_SPEED * FIXED_DELTA_TIME * (1.0f / 80.0f), &deltaRotate);
     struct Quaternion tempRotation;
     quatMultiply(&deltaRotate, &scene->camera.transform.rotation, &tempRotation);
 
     // pitch
-    quatAxisAngle(&gRight, pad->stick_y * ROTATE_SPEED * FIXED_DELTA_TIME * (1.0f / 80.0f), &deltaRotate);
+    quatAxisAngle(&gRight, stickY * ROTATE_SPEED * FIXED_DELTA_TIME * (1.0f / 80.0f), &deltaRotate);
     quatMultiply(&tempRotation, &deltaRotate, &scene->camera.transform.rotation);
 
     scene->verticalVelocity += GRAVITY * FIXED_DELTA_TIME;
